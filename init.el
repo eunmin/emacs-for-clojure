a4819bdf37c607120a7c8c8738c7b53fd3b8f15d
Eunmin Kim <telepopsound@gmail.com>
2020-05-23 22:37:24 +0900

    Fix bug

--- init.el
+++ init.el
@@ -159,21 +159,6 @@
 (defun nrepl-reset ()
   (interactive)
   (cider-execute "(reset)")
-  (message "Reset")
+  (message "Reset"))
 
 (define-key cider-mode-map (kbd "C-c r") 'nrepl-reset)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (company clj-refactor cider clojure-mode rainbow-delimiters paredit color-theme-sanityinc-tomorrow use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
